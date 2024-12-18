f9cd9fb00cde45da05547b77d82048aa08ea7378
rohits06oct <49077126+rohits06oct@users.noreply.github.com>
2024-12-18 23:01:27 +0530

    Update TestRunner.java

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -1,8 +1,6 @@
 package Runners;
 
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 
 @CucumberOptions(
         features = "src/test/resources/Features",
@@ -10,20 +8,8 @@ import org.testng.annotations.BeforeSuite;
         monochrome = true,
         plugin = {
                 "pretty",
-                "html:target/cucumber-reports/cucumber-pretty",
-                "json:target/cucumber-reports/CucumberTestReport.json",
-                "timeline:target/test-output-thread/"
+                "html:target/cucumber-reports"
         }
 )
 public class TestRunner {
-
-    @BeforeSuite
-    public void beforeSuite() {
-        System.out.println("================ BEFORE SUITE ================");
-    }
-
-    @AfterSuite
-    public void afterSuite() {
-        System.out.println("================ AFTER SUITE ================");
-    }
 }
